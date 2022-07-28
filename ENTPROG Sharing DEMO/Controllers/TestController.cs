e3fa37929f5dac1d33b0e13546403993d9e0a3e7
biancaalbax <tonibianca.ortega@benilde.edu.ph>
2022-07-28 10:01:58 +0800

    Added TestController

---
+++ ENTPROG Sharing DEMO/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ENTPROG_Sharing_DEMO.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
